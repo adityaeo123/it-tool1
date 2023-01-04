ec624c612c83095d628420e98fc92ec3889aa2cd
adityaeo123 <121929569+adityaeo123@users.noreply.github.com>
2023-01-04 13:31:55 +0530

    Add files via upload

---
+++ fyit 105.py
@@ -0,0 +1,22 @@
+def my_function(roll_no):
+    print(roll_no,"FYIT")
+my_function(101)
+my_function(102)
+my_function(109)
+
+
+
+
+
+
+def add(a,b):
+    sum = a + b
+    result = print("sum of given number is",sum)
+    return result
+
+x= int(input("Enter a first number: "))
+y= int(input("Enter a second number: "))
+
+add(x,y)
+
+       
